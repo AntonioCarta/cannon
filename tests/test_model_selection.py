4cb826a8111835a67f985173c07f20666350d05b
AntonioCarta <a.carta@outlook.it>
2018-04-18 13:59:26 +0200

    add tests

---
+++ tests/test_model_selection.py
@@ -0,0 +1,49 @@
+import sys
+sys.path.append('./src')
+from cannon.experiment import ModelSelectionConfig, ModelSelection
+
+
+class MockData():
+    def get_data(self):
+        return [], []
+
+
+class MockModel:
+    def __init__(self, x):
+        self.best_result = {
+                'tr_loss': x,
+                'tr_acc': x,
+                'vl_loss': x,
+                'vl_acc': x
+            }
+
+    def fit(self, tr, vl):
+        return [self.best_result['tr_loss']], [self.best_result['vl_loss']]
+
+
+class MockBuilder:
+    def __init__(self):
+        pass
+
+    def build_model(self, params):
+        m = MockModel(params['x'])
+        return m
+
+    def generate_params(self, i):
+        return {'x': i+1}
+
+    def total_grid_configurations(self):
+        return len(self.h_iter) * len(self.m_iter)
+
+
+def test_grid_search():
+    exp = ModelSelection(log_dir='./logs/debug/')
+    builder = MockBuilder()
+    config = ModelSelectionConfig(builder, MockData(), MockData(), 0, 10, 3)
+
+    exp.run(config)
+    assert exp.best_results['vl_loss_avg'] == 0
+
+
+if __name__ == '__main__':
+    test_grid_search()
---
+++ tests/test_piano_roll.py
@@ -0,0 +1,39 @@
+import sys
+sys.path.append('./src')
+from cannon.tasks import PianoRollData
+import torch
+
+
+def test_piano_roll():
+    train_dataset = PianoRollData('./data/MIDI/piano-roll/JSB Chorales.pickle', 'train', timesteps=50)
+    # test one_hot_list generator
+    print("test data loader")
+    for song_x, song_y in train_dataset.get_one_hot_list():
+        assert torch.equal(song_x[1:], song_y[:-1])
+
+    # test get_data
+    X_train, y_train, masks = train_dataset.get_data()
+    print("test data loader")
+    for i in range(0, X_train.size(1)):
+        X_i, y_i = X_train[:, i, :], y_train[:, i, :]
+        mask_i = masks[:, i]
+        max_idx = int(mask_i.sum().data.numpy()[0])  # need to keep track of padding
+        assert torch.equal(X_i[1:max_idx], y_i[:max_idx-1])
+
+
+def test_masking():
+    train_dataset = PianoRollData('./data/MIDI/piano-roll/JSB Chorales.pickle', 'train', timesteps=50)
+    batch_size = 32
+    X_train, y_train, masks = train_dataset.get_data()
+    print("test masking")
+    for i in range(0, X_train.size(1), batch_size):
+        y_i = y_train[:, i:i + batch_size, :]
+        mask_i = masks[:, i:i + batch_size]
+        y_mask = y_i * mask_i.expand_as(y_i)
+        assert torch.equal(y_i, y_mask)
+
+
+if __name__ == '__main__':
+    test_piano_roll()
+    test_masking()
+    print("test DONE.")
