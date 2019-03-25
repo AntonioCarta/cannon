4c6720278018fcd80d587b7d0408715181e2174a
antonio <antonio.carta@di.unipi.it>
2019-03-25 11:26:55 +0100

    add utilities for logging and tagging deprecated functions

--- cannon/tables.py
+++ cannon/tables.py
@@ -9,11 +9,14 @@ def load_dir_results(log_dir):
         if os.path.isdir(file):
             log_file = log_dir + file.name + '/checkpoint.pickle'
 
-            with open(log_file, 'rb') as f:
-                d = pickle.load(f)
-                best_result = d['best_result']
-                train_par = d['train_params']
-                model_par = d['model_params']
+            try:
+                with open(log_file, 'rb') as f:
+                    d = pickle.load(f)
+                    best_result = d['best_result']
+                    train_par = d['train_params']
+                    # model_par = d['model_params']
 
-            res.append((best_result, train_par, model_par))
+                res.append((best_result, train_par)) #, model_par))
+            except EOFError:
+                print(f"could not open {log_file}")
     return res
--- cannon/utils.py
+++ cannon/utils.py
@@ -3,8 +3,60 @@ import time
 import torch
 import numpy as np
 from torch.autograd import Variable
+import functools
+import inspect
+import warnings
 
 ALLOW_CUDA = True  # Global variable to control cuda_move allocation behavior
+
+
+def deprecated(reason):
+    """
+    This is a decorator which can be used to mark functions
+    as deprecated. It will result in a warning being emitted
+    when the function is used.
+    source: https://stackoverflow.com/questions/2536307/decorators-in-the-python-standard-lib-deprecated-specifically
+    """
+    string_types = (type(b''), type(u''))
+    if isinstance(reason, string_types):
+        def decorator(func1):
+
+            if inspect.isclass(func1):
+                fmt1 = "Call to deprecated class {name} ({reason})."
+            else:
+                fmt1 = "Call to deprecated function {name} ({reason})."
+
+            @functools.wraps(func1)
+            def new_func1(*args, **kwargs):
+                warnings.simplefilter('always', DeprecationWarning)
+                warnings.warn(
+                    fmt1.format(name=func1.__name__, reason=reason),
+                    category=DeprecationWarning,
+                    stacklevel=2
+                )
+                warnings.simplefilter('default', DeprecationWarning)
+                return func1(*args, **kwargs)
+            return new_func1
+        return decorator
+    elif inspect.isclass(reason) or inspect.isfunction(reason):
+        func2 = reason
+        if inspect.isclass(func2):
+            fmt2 = "Call to deprecated class {name}."
+        else:
+            fmt2 = "Call to deprecated function {name}."
+        @functools.wraps(func2)
+        def new_func2(*args, **kwargs):
+            warnings.simplefilter('always', DeprecationWarning)
+            warnings.warn(
+                fmt2.format(name=func2.__name__),
+                category=DeprecationWarning,
+                stacklevel=2
+            )
+            warnings.simplefilter('default', DeprecationWarning)
+            return func2(*args, **kwargs)
+        return new_func2
+    else:
+        raise TypeError(repr(type(reason)))
 
 
 def set_allow_cuda(b):
