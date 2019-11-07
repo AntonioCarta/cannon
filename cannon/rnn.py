493c61444ecaaf164956a683168225d9d2acb4c5
antonio <antonio.carta@di.unipi.it>
2019-11-07 15:41:21 +0100

    standard initialization. Remove unused decoder in LMN.

--- cannon/rnn.py
+++ cannon/rnn.py
@@ -14,22 +14,9 @@
 import torch
 from torch import jit
 from torch import nn
-from torch.jit import Tensor
+from torch import Tensor
 from typing import Tuple
 from .utils import standard_init
-
-
-class LMNDecoder(jit.ScriptModule):
-    def __init__(self, hidden_size, memory_size):
-        super().__init__()
-        self.C = nn.Linear(memory_size, hidden_size)
-        self.D = nn.Linear(memory_size, memory_size)
-
-    @jit.script_method
-    def forward(self, last_m: Tensor) -> Tuple[Tensor, Tensor]:
-        prev_h = self.C(last_m)
-        last_m = self.D(last_m)
-        return prev_h, last_m
 
 
 class LinearMemoryNetwork(jit.ScriptModule):
@@ -40,12 +27,13 @@ class LinearMemoryNetwork(jit.ScriptModule):
         self.out_hidden = out_hidden
         self.memory_size = memory_size
         self.act = act
-        self.Wxh = nn.Parameter(0.01 * torch.randn(hidden_size, in_size))
-        self.Whm = nn.Parameter(0.01 * torch.randn(memory_size, hidden_size))
-        self.Wmm = nn.Parameter(0.01 * torch.randn(memory_size, memory_size))
-        self.Wmh = nn.Parameter(0.01 * torch.randn(hidden_size, memory_size))
-        self.bh = nn.Parameter(0.01 * torch.randn(hidden_size))
-        self.bm = nn.Parameter(0.01 * torch.randn(memory_size))
+        self.Wxh = nn.Parameter(torch.randn(hidden_size, in_size))
+        self.Whm = nn.Parameter(torch.randn(memory_size, hidden_size))
+        self.Wmm = nn.Parameter(torch.randn(memory_size, memory_size))
+        self.Wmh = nn.Parameter(torch.randn(hidden_size, memory_size))
+        self.bh = nn.Parameter(torch.randn(hidden_size))
+        self.bm = nn.Parameter(torch.randn(memory_size))
+        standard_init(self.parameters())
 
     @jit.script_method
     def init_hidden(self, batch_size: int) -> Tensor:
@@ -59,27 +47,11 @@ class LinearMemoryNetwork(jit.ScriptModule):
         out = h_curr if self.out_hidden else m_curr
         return out, m_curr
 
-    @jit.script_method
-    def encode(self, x_prev, m_prev):
-        assert len(x_prev.shape) == 2
-        h_curr = self.act(torch.mm(x_prev, self.Wxh.t()) + torch.mm(m_prev, self.Wmh.t()) + self.bh)
-        m_curr = torch.mm(h_curr, self.Whm.t()) + torch.mm(m_prev, self.Wmm.t()) + self.bm
-        return h_curr, m_curr
-
 
 class LMNLayer(jit.ScriptModule):
-    def __init__(self, in_size, hidden_size, memory_size, act=torch.tanh, out_hidden=False, tie_decoder_weights=False):
+    def __init__(self, in_size, hidden_size, memory_size, act=torch.tanh, out_hidden=False):
         super().__init__()
         self.layer = LinearMemoryNetwork(in_size, hidden_size, memory_size, act=act, out_hidden=out_hidden)
-
-        # only useful for decoding
-        self.decoder = LMNDecoder(hidden_size, memory_size)
-        if tie_decoder_weights:
-            self.decoder.C.weight.data = self.layer.Whm.t()
-            self.decoder.D.weight.data = self.layer.Wmm.t()
-            self.ae_params = [self.layer.Whm, self.layer.Wmm, self.layer.bm]
-        else:
-            self.ae_params = [self.layer.Whm, self.layer.Wmm, self.layer.bm] + list(self.decoder.parameters())
         standard_init(self.parameters())
 
     @jit.script_method
@@ -96,24 +68,6 @@ class LMNLayer(jit.ScriptModule):
             h_prev, m_prev = self.layer(xt, m_prev)
             out.append(h_prev)
         return torch.stack(out), m_prev
-
-    @jit.script_method
-    def hidden_reconstruction(self, x):
-        assert len(x.shape) == 3
-        m_prev = self.init_hidden(x.shape[1])
-        hl = []
-        x = x.unbind(0)
-        # encode
-        for t in range(len(x)):
-            xt = x[t]
-            h_prev, m_prev = self.layer.encode(xt, m_prev)
-            hl.append(h_prev.detach())
-        # decode
-        reco = []
-        for t in range(len(x)):
-            h_prev, m_prev = self.decoder(m_prev)
-            reco = [h_prev] + reco
-        return torch.stack(hl, dim=0), torch.stack(reco, dim=0)
 
 
 class RNNCell(jit.ScriptModule):
